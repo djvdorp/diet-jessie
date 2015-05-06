fe91c1bb5fa967995a4574dc1b5bfbca7d85bf2a
Daniel van Dorp <daniel@vandorp.biz>
2015-05-06 20:53:13 +0200

    fixes sources.list

--- setup.sh
+++ setup.sh
@@ -6,3 +6,17 @@ ESSENTIALS="bash curl wget axel screen htop nmon"
 
 ## 1. Desktop Environment and Window Manager
 DESKTOP_ENV="xorg i3 lightdm"
+
+
+
+cat > /etc/apt/sources.list <<EOF
+deb http://httpredir.debian.org/debian/ jessie main contrib non-free
+deb-src http://httpredir.debian.org/debian/ jessie main contrib non-free
+
+deb http://security.debian.org/ jessie/updates main contrib non-free
+deb-src http://security.debian.org/ jessie/updates main contrib non-free
+
+# jessie-updates, previously known as 'volatile'
+deb http://httpredir.debian.org/debian/ jessie-updates main contrib non-free
+deb-src http://httpredir.debian.org/debian/ jessie-updates main contrib non-free
+EOF
