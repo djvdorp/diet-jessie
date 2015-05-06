fa28ffab9f5707652dcf3a14ef38c0fb110dd77a
Daniel van Dorp <daniel@vandorp.biz>
2015-05-06 20:32:03 +0200

    some first package declarations

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Package declarations:
+## 0. Bare essentials
+ESSENTIALS="bash curl wget axel screen htop nmon"
+
+## 1. Desktop Environment and Window Manager
+DESKTOP_ENV="xorg i3 lightdm"
